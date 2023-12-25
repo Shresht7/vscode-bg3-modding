2bc2d4ab8932eb51079fa298c7c89682e98727ea
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-25 18:07:03 +0530

    Add Activation Event for when `meta.lsx` is detected in the workspace

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,10 @@ import { providers } from './providers';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+// or when `meta.lsx` file is detected in the workspace 
 export function activate(context: vscode.ExtensionContext) {
+	// Show Information Message when the extension is activated
+	vscode.window.showInformationMessage("BG3 Modding Extension Activated!");
 	// Register all the commands and providers, and subscribe to their disposables
 	context.subscriptions.push(
 		...commands,
