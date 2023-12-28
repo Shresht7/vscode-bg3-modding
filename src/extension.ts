b85cd14b03c4b10e8e7815a55daba3d3804854f8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-29 00:14:23 +0530

    Read and parse the `meta.lsx` file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // Library
 // The module `vscode` contains the VS Code extensibility API
 import * as vscode from 'vscode';
+import { XMLParser } from 'fast-xml-parser';
 
 // Commands
 import { commands } from './commands';
@@ -8,17 +9,30 @@ import { commands } from './commands';
 // Providers
 import { providers } from './providers';
 
+// Helpers
+import { fs } from './helpers';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 // or when `meta.lsx` file is detected in the workspace 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+
 	// Show Information Message when the extension is activated
 	vscode.window.showInformationMessage("BG3 Modding Extension Activated!");
+
+	// Get the path to the `meta.lsx` file
+	const metaLsxFile = (await fs.findMetaLsxUris(1))[0];
+	const fileBuffer = await vscode.workspace.fs.readFile(metaLsxFile);
+	const fileContents = Buffer.from(fileBuffer).toString("utf8");
+
+	const meta = new XMLParser({ ignoreAttributes: false }).parse(fileContents);
+
 	// Register all the commands and providers, and subscribe to their disposables
 	context.subscriptions.push(
 		...commands,
 		...providers
 	);
+
 }
 
 // This method is called when your extension is deactivated
