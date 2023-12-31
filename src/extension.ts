17666b7daeb51a9b2e27c012a54b4d903fb0027d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-31 23:50:05 +0530

    Create `FileSystemWatcher` for localization xml files

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import { commands } from './commands';
 
 // Providers
 import { providers } from './providers';
+import { buildLocalizationReference } from './library/bg3/buildLocalizationReference';
 
 
 // This method is called when your extension is activated
@@ -16,6 +17,29 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	// Show Information Message when the extension is activated
 	vscode.window.showInformationMessage("BG3 Modding Extension Activated!");
+
+	// Create a file-watcher for the xml localization files
+	const localizationXMLFileWatcher = vscode.workspace.createFileSystemWatcher("**/Localization/**/*.xml");
+
+	// Build localization references when a new xml is created
+	localizationXMLFileWatcher.onDidCreate(e => {
+		buildLocalizationReference([e]);
+	});
+
+	// Rebuild localization references when a xml is changed
+	// ? Performance concerns?
+	localizationXMLFileWatcher.onDidChange(e => {
+		buildLocalizationReference([e]);
+	});
+
+	// Remove localization references when a xml is deleted
+	localizationXMLFileWatcher.onDidDelete(e => {
+		/**
+		 * // TODO: Remove references. May also have to dispose off the registered hover provider
+		 * ? (if I decide to register separate providers for each file)
+		 */
+		throw new Error("TODO: Implementation pending!");
+	});
 
 	// Register all the commands and providers, and subscribe to their disposables
 	context.subscriptions.push(
