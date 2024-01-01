8592bfb9afe438cce29a4c711aa33009c22a5b7c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-01 13:14:43 +0530

    Refactor `VersionHoverProvider`

--- src/providers/hover/version.ts
+++ src/providers/hover/version.ts
@@ -24,15 +24,17 @@ export class VersionHoverProvider extends HoverProvider {
         this.register();
     }
 
+    // PROVIDE HOVER
+    // -------------
+
     override provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
 
         // Get the line that is currently being hovered over
         const line = document.lineAt(position.line);
-        // Check if the line matches the version number regex
-        if (!VersionHoverProvider.versionRegex.test(line.text)) { return; };
 
         // Capture the int64 version number from the line and parse as BigInt
-        const capture = VersionHoverProvider.versionRegex.exec(line.text)?.at(1) || '0';
+        const capture = this.execRegex(line.text);
+        if (!capture) { return; }
         const bigIntVersion = BigInt(capture);
 
         // Get the word that is being currently hovered over
@@ -41,10 +43,26 @@ export class VersionHoverProvider extends HoverProvider {
 
         // If hovering over the version number, show its string representation
         if (word === capture) {
-            const version = new bg3.Version(bigIntVersion);
-            return new vscode.Hover(version.toString());
+            const version = this.transformVersion(bigIntVersion);
+            return new vscode.Hover(version);
         }
 
+    }
+
+    // HELPERS
+    // -------
+
+    /** Transform version number from BigInt representation (e.g. ) to a string representation (e.g. 1.0.0.0) */
+    private transformVersion(bigIntVersion: bigint): string {
+        return new bg3.Version(bigIntVersion).toString();
+    }
+
+    /** Perform the regex operation and capture the version64 number */
+    private execRegex(line: string): string | undefined {
+        // Check if the line matches the version regex
+        if (!VersionHoverProvider.versionRegex.test(line)) { return; };
+        // Capture version64 from the line
+        return VersionHoverProvider.versionRegex.exec(line)?.at(1);
     }
 
 };
