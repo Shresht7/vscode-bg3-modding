9bd7a12782d70ab02988546eb2d224dfa97cd562
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-03 02:17:12 +0530

    Add `Publisher` information

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,10 @@ const constants = {
     /** ID of the extension. Must match the `package.json` */
     EXTENSION_ID: "bg3-modding",
 
+    /** Publisher */
+    // TODO: This might need to change in the future based on availability of the publisher name on the VS Code Marketplace
+    PUBLISHER: "Shresht7",
+
     /** The folder name that contains the Lua workspace library references */
     LUA_REFERENCES_FOLDER: "references",
 
