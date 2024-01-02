333a3dd9ed7ca7c258f28ed90be51a41ecd349bf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-02 16:19:43 +0530

    Add error handling to `copyModUUID`

--- src/commands/copyModUUID.ts
+++ src/commands/copyModUUID.ts
@@ -2,10 +2,21 @@
 import * as vscode from 'vscode';
 import { bg3 } from "../library";
 
+// ---------------------
+// COPY MOD UUID COMMAND
+// ---------------------
+
 /** Copies the mod's UUID to the clipboard */
 export async function copyModUUID() {
+
     // Get mod's metadata from the `meta.lsx` file
-    const meta = await bg3.metaLsx.parse();
+    let meta: bg3.MetaLsx;
+    try {
+        meta = await bg3.metaLsx.parse();
+    } catch (e) {
+        vscode.window.showErrorMessage("No `meta.lsx` file found in the workspace");
+        return;
+    }
 
     // Copy the UUID to the clipboard
     const uuid = meta.ModuleInfo.UUID;
@@ -14,4 +25,5 @@ export async function copyModUUID() {
     // Show an information message notifying the user about the action
     const modName = meta.ModuleInfo.Name;
     vscode.window.showInformationMessage(`📋 ${modName}'s UUID copied to clipboard! -- ${uuid}`);
+
 }
