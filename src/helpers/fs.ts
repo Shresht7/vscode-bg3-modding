71891374236f0ebb0ebb74d03add5629963a0605
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-03 01:51:43 +0530

    Create helper function to `getExtensionPath`

---
+++ src/helpers/fs.ts
@@ -0,0 +1,23 @@
+// Library
+import * as vscode from 'vscode';
+import constants from '../constants';
+
+// -----------
+// FILE-SYSTEM
+// -----------
+
+/**
+ * Get the path to the extension's directory
+ * @returns {Promise<string>} The path to the extension's directory
+ * @throws {Error} If the extension is not found
+ * @example
+ * const extensionPath = await getExtensionPath();
+ * const pathToMyFile = path.join(extensionPath, 'myFile.txt');
+ */
+export async function getExtensionPath(): Promise<string> {
+    const extension = vscode.extensions.getExtension(constants.EXTENSION_ID);
+    if (!extension) {
+        throw new Error('Extension not found');
+    }
+    return extension.extensionPath;
+}
