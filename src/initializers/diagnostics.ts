ffc6044e1fc9da4dc9d47467519540323c949690
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-13 00:30:16 +0530

    Implement `determineLineFromProperty`

--- src/initializers/diagnostics.ts
+++ src/initializers/diagnostics.ts
@@ -142,8 +142,53 @@ class LocalizationXMLDiagnostics extends Diagnostics {
         return problems;
     }
 
+    /**
+     * Determine the line of the document that the property corresponds to
+     * @param document The document to check
+     * @param property The property to check. (e.g. `instance.contentList.content[2]`)
+     * @returns The line of the document that the property corresponds to
+     */
     private determineLineFromProperty(document: vscode.TextDocument, property: string): number {
-        return 0; // TODO: Implement this method to determine the line from the property path
+        /** The line of the document that the property corresponds to */
+        let line = 0;
+
+        /** string[] of the parts of the property path */
+        const parts = property.split(".").slice(1);
+
+        // Iterate through the lines of the document to find the line that the property corresponds to
+        /** The current part of the property path */
+        let part = parts.shift();
+        for (let i = 0; i < document.lineCount; i++) {
+            // If the part is undefined or "_@_", then we break out of the loop and return the latest line
+            if (part === undefined || part === "_@_") { break; }
+
+            /** The text corresponding to the current line */
+            const text = document.lineAt(i).text;
+
+            // If the part is an array...
+            if (/\[(\d+)\]/.test(part)) {
+                const match = /\[(\d+)\]/.exec(part);
+                const index = parseInt(match![1]);
+                // ...continue until we find the correct index
+                if (index > 0) {
+                    const newIndex = index - 1;
+                    part = part.replace(match![0], "[" + newIndex + "]");
+                    continue;
+                } else if (index === 0) {
+                    // If the index is 0, then we select the next occurrence of the part
+                    part = part.replace(match![0], "");
+                }
+            }
+
+            // If the text includes the part, then we set the line to the current line and move on to the next part
+            if (text.includes("<" + part)) {
+                line = i;
+                part = parts.shift();
+            }
+        }
+
+        // Return the line
+        return line;
     }
 
 }
