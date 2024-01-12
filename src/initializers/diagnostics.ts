f719da4a9651cd56c19c6934b1b2ec7dabed73f1
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-12 23:57:18 +0530

    Extract the localization schema out

--- src/initializers/diagnostics.ts
+++ src/initializers/diagnostics.ts
@@ -1,7 +1,10 @@
 // Library
 import * as vscode from 'vscode';
 import { XMLParser } from 'fast-xml-parser';
-import { Schema, Validator } from 'jsonschema';
+import { Validator } from 'jsonschema';
+
+// Schemas
+import { localizationXMLSchema } from '../library/bg3/schema';
 
 const jsonValidator = new Validator();
 
@@ -144,54 +147,3 @@ class LocalizationXMLDiagnostics extends Diagnostics {
     }
 
 }
-
-/** The schema for localization XML files */
-const localizationXMLSchema: Schema = {
-    type: "object",
-    required: ["?xml", "contentList"],
-    properties: {
-        "?xml": {
-            type: "object",
-            properties: {
-                "_@_": {
-                    type: "object",
-                    required: ["version", "encoding"],
-                    properties: {
-                        version: { type: "number" },
-                        encoding: { type: "string" },
-                    }
-                }
-            }
-        },
-        contentList: {
-            type: "object",
-            required: ["content"],
-            properties: {
-                content: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        properties: {
-                            "_@_": {
-                                type: "object",
-                                required: ["contentuid", "version"],
-                                properties: {
-                                    contentuid: {
-                                        type: "string",
-                                        pattern: "^h[a-f\\d]{8}(g[a-f\\d]{4}){3}g[a-f\\d]{12}$",
-                                    },
-                                    version: {
-                                        type: "number",
-                                    },
-                                    "#text": {
-                                        type: "string",
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-};
---
+++ src/library/bg3/schema/localization.schema.ts
@@ -0,0 +1,53 @@
+// Type Definitions
+import type { Schema } from "jsonschema";
+
+/** The schema for localization XML files */
+export const localizationXMLSchema: Schema = {
+    type: "object",
+    required: ["?xml", "contentList"],
+    properties: {
+        "?xml": {
+            type: "object",
+            properties: {
+                "_@_": {
+                    type: "object",
+                    required: ["version", "encoding"],
+                    properties: {
+                        version: { type: "number" },
+                        encoding: { type: "string" },
+                    }
+                }
+            }
+        },
+        contentList: {
+            type: "object",
+            required: ["content"],
+            properties: {
+                content: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            "_@_": {
+                                type: "object",
+                                required: ["contentuid", "version"],
+                                properties: {
+                                    contentuid: {
+                                        type: "string",
+                                        pattern: "^h[a-f\\d]{8}(g[a-f\\d]{4}){3}g[a-f\\d]{12}$",
+                                    },
+                                    version: {
+                                        type: "number",
+                                    },
+                                    "#text": {
+                                        type: "string",
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
