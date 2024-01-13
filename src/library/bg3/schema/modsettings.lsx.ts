81de3f938cb441829baf4b80cb2a13e07b910773
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-13 16:07:18 +0530

    Add `ModOrderSchema`

--- src/library/bg3/schema/modsettings.lsx.ts
+++ src/library/bg3/schema/modsettings.lsx.ts
@@ -35,6 +35,72 @@ const versionSchema: Schema = {
     }
 };
 
+const ModOrderSchema: Schema = {
+    type: "object",
+    required: [attributeGroupName, "children"],
+    properties: {
+        [attributeGroupName]: {
+            type: "object",
+            required: ["id"],
+            properties: {
+                id: { type: "string", const: "ModOrder" },
+            }
+        },
+        children: {
+            type: "object",
+            required: ["node"],
+            properties: {
+                node: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        required: [attributeGroupName, "attribute"],
+                        properties: {
+                            [attributeGroupName]: {
+                                type: "object",
+                                required: ["id"],
+                                properties: {
+                                    id: { type: "string", const: "Module" },
+                                }
+                            },
+                            attribute: {
+                                type: "object",
+                                required: [attributeGroupName],
+                                properties: {
+                                    [attributeGroupName]: {
+                                        type: "object",
+                                        required: ["id", "value", "type"],
+                                        properties: {
+                                            id: { type: "string", const: "UUID" },
+                                            value: { type: "string", pattern: "^[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}$" },
+                                            type: { type: "string", const: "FixedString" },
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+const ModsSchema: Schema = {
+    type: "object",
+    required: [attributeGroupName, "children"],
+    properties: {
+        [attributeGroupName]: {
+            type: "object",
+            required: ["id"],
+            properties: {
+                id: { type: "string", const: "Mods" },
+            }
+        },
+        children: {}
+    }
+};
+
 /** The schema for `modsettings.lsx` file */
 export const modsettingsLSXSchema: Schema = {
     type: "object",
@@ -68,6 +134,20 @@ export const modsettingsLSXSchema: Schema = {
                                         id: { type: "string", const: "root" },
                                     }
                                 },
+                                children: {
+                                    type: "object",
+                                    required: ["node"],
+                                    properties: {
+                                        node: {
+                                            type: "array",
+                                            items: [
+                                                ModOrderSchema,
+                                                ModsSchema
+                                            ],
+                                            uniqueItems: true
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
