8394850d06d892a1ae8b096f92f3f44f5d760284
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-14 18:03:06 +0530

    Refactor `localizationXMLSchema`

--- src/library/bg3/schema/localization.xml.ts
+++ src/library/bg3/schema/localization.xml.ts
@@ -7,10 +7,51 @@ import { xmlDeclarationSchema } from "./partials";
 // Type Definitions
 import type { Schema } from "jsonschema";
 
+// --------
+// PARTIALS
+// --------
+
+/**
+ * The schema for the content element
+ * ```xml
+ * <content contentuid="_uuid_" version="1">_Localized-text_</content>
+ * ```
+ */
+const contentSchema: Schema = {
+    type: "array",
+    items: {
+        type: "object",
+        properties: {
+            [attributesGroupName]: {
+                type: "object",
+                required: ["contentuid", "version"],
+                properties: {
+                    contentuid: {
+                        type: "string",
+                        pattern: "^h[a-f\\d]{8}(g[a-f\\d]{4}){3}g[a-f\\d]{12}$",
+                    },
+                    version: {
+                        type: "number",
+                        minimum: 0,
+                    },
+                    "#text": {
+                        type: "string",
+                    }
+                }
+            }
+        }
+    }
+};
+
+// -----------------------
+// LOCALIZATION XML SCHEMA
+// -----------------------
+
 /** The schema for localization XML files */
 export const localizationXMLSchema: Schema = {
     type: "object",
     required: ["?xml", "contentList"],
+    additionalProperties: false,
     properties: {
         "?xml": xmlDeclarationSchema,
         contentList: {
@@ -18,31 +59,7 @@ export const localizationXMLSchema: Schema = {
             required: ["content"],
             additionalProperties: false,
             properties: {
-                content: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        properties: {
-                            [attributesGroupName]: {
-                                type: "object",
-                                required: ["contentuid", "version"],
-                                properties: {
-                                    contentuid: {
-                                        type: "string",
-                                        pattern: "^h[a-f\\d]{8}(g[a-f\\d]{4}){3}g[a-f\\d]{12}$",
-                                    },
-                                    version: {
-                                        type: "number",
-                                        minimum: 0,
-                                    },
-                                    "#text": {
-                                        type: "string",
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                content: contentSchema,
             }
         }
     }
