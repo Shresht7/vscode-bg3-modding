c79d41cdc2e11fbcaaf2a75594eb4217cf276635
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-14 17:52:55 +0530

    Update schema definition

--- src/library/bg3/schema/partials/xmlDeclaration.ts
+++ src/library/bg3/schema/partials/xmlDeclaration.ts
@@ -8,15 +8,28 @@ import type { Schema } from "jsonschema";
 // XML DECLARATION
 // ---------------
 
+/** 
+ * The schema for the XML declaration line
+ * ```xml
+ * <?xml version="1.0" encoding="utf-8"?>
+ * ```
+*/
 export const xmlDeclarationSchema: Schema = {
     type: "object",
     properties: {
         [attributesGroupName]: {
             type: "object",
             required: ["version", "encoding"],
+            additionalProperties: false,
             properties: {
-                version: { type: "number" },
-                encoding: { type: "string" },
+                version: {
+                    type: "number",
+                    minimum: 1,
+                },
+                encoding: {
+                    type: "string",
+                    enum: ["utf-8", "UTF-8"],
+                },
             }
         }
     }
