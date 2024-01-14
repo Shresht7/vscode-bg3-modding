32b097e001c29f720e6a5a03104fdee0a6a34a4a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-15 01:55:25 +0530

    Scaffold diagnostics for `meta.lsx`

---
+++ src/library/bg3/schema/meta.lsx.ts
@@ -0,0 +1,11 @@
+// Type Definitions
+import type { Schema } from "jsonschema";
+
+// ---------------
+// META LSX SCHEMA
+// ---------------
+
+/** The schema for meta lsx files */
+export const metaLSXSchema: Schema = {
+
+};
